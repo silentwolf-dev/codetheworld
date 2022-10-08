4184683a4b746bf5de74139e8b886b49345cfd8b
silentwolf-dev <omariimanibrown421@gmail.com>
2022-10-07 22:56:07 -0400

    added search

--- src/components/CourseSearch.jsx
+++ src/components/CourseSearch.jsx
@@ -1,5 +1,26 @@
 /** @format */
+import ".././css/components/searchbar.css";
 
 export const CourseSearch = ({ buttonTitle, placeholder, courselist }) => {
-	return <div></div>;
+	return (
+		<div className='searchbar'>
+			<div className='search-input-container center'>
+				<input type='text' className='search-input' placeholder="search javaScript, CSS, etc..." />
+				<button className="search-btn">search <span className="material-icons">search</span></button>
+			</div>
+			<ul className='search-content-container center hidden-sc'>
+				{courselist.map((v) => {
+					return (
+						<li className='search-content'>
+							<div>
+							<h3 class='search-content-title'>{v.name}</h3>
+							<p className='search-content-text'>{v.description}</p>
+							</div>
+							<img className="search-content-img" src={v.img} alt='' />
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,23 +1,34 @@
-import '../../css/pages/home.css'
-import { Link } from 'react-router-dom'
-import { CourseSearch } from '../CourseSearch.jsx'
-import { courses } from '../../content/courses'
+/** @format */
 
+import "../../css/pages/home.css";
+import { Link } from "react-router-dom";
+import { CourseSearch } from "../CourseSearch.jsx";
+import { courses } from "../../content/courses";
+import "../../css/pages/home.css"
 
-
+const tags = ["css", "c#", "javascript", "html"];
 
 export const Home = (props) => {
-  return (
-    <div className="homepage">
-        <div className='home-intro-container'>
-            <h1>Welcome to codeMastery</h1>
-            <p>Search for a course or visit <Link to="/explore">Explore</Link>to view courses</p>
-            <CourseSearch
-              placeholder={"search something"}
-              courselist={courses}
-              buttonTitle={"search"}
-            />
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className='homepage'>
+			<div className='home-intro-container'>
+				<h1>Welcome to codeMastery</h1>
+				<p>
+				  You can find over 100+ courses based on what on your topic or visit <Link to='/explore'>Explore</Link>to view
+					courses
+				</p>
+				<CourseSearch
+					placeholder={"search something"}
+					courselist={courses}
+					buttonTitle={"search"}
+				/>
+
+				<ul className='home-taglist-container'>
+					{tags.map((v) => {
+						return <li className="home-taglist">{v}</li>;
+					})}
+				</ul>
+			</div>
+		</div>
+	);
+};
